97604bdb25cf000469f6479a8a266dff96e24a67
Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
2011-05-25 15:11:41 +0000

    initial import
    git-svn-id: svn+ssh://svn.cern.ch/reps/acc-co/trunk/accmodel/jmad@191668 e02d3557-2f25-4247-9e18-1207d715a173

---
+++ jmad-models/jmad-models-medaustron/src/test/cern/jmad/modeldefs/ModelDefCreator.java
@@ -0,0 +1,50 @@
+package cern.jmad.modeldefs;
+
+import java.io.File;
+
+import org.apache.log4j.BasicConfigurator;
+
+import cern.accsoft.steering.jmad.modeldefs.ModelDefinitionFactory;
+import cern.accsoft.steering.jmad.modeldefs.domain.JMadModelDefinition;
+import cern.accsoft.steering.jmad.modeldefs.io.ModelDefinitionPersistenceService;
+import cern.accsoft.steering.jmad.modeldefs.io.impl.ModelDefinitionUtil;
+import cern.accsoft.steering.jmad.modeldefs.io.impl.XmlModelDefinitionPersistenceService;
+import cern.accsoft.steering.jmad.util.xml.PersistenceServiceException;
+import cern.jmad.modeldefs.defs.MedAustronModelDefinitionFactory;
+
+public class ModelDefCreator {
+
+    public final static void main(String[] args) {
+
+        String destPath = "";
+        if (args.length > 0) {
+            destPath = args[0];
+        }
+
+        BasicConfigurator.configure();
+
+        ModelDefinitionFactory[] factories = new ModelDefinitionFactory[] { new MedAustronModelDefinitionFactory() };
+
+        ModelDefinitionPersistenceService service = new XmlModelDefinitionPersistenceService();
+
+        for (ModelDefinitionFactory factory : factories) {
+            JMadModelDefinition modelDefinition = factory.create();
+            String fileName = ModelDefinitionUtil.getProposedXmlFileName(modelDefinition);
+            String filePath;
+            if (destPath.length() > 0) {
+                filePath = destPath + "/" + fileName;
+            } else {
+                filePath = fileName;
+            }
+            File file = new File(filePath);
+            System.out.println("Writing file '" + file.getAbsolutePath() + "'.");
+            try {
+                service.save(modelDefinition, file);
+            } catch (PersistenceServiceException e) {
+                System.out.println("Could not save model definition to file '" + file.getAbsolutePath());
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
---
+++ jmad-models/jmad-models-medaustron/src/test/cern/jmad/modeldefs/defs/MedAustronModelDefinitionFactory.java
@@ -0,0 +1,58 @@
+package cern.jmad.modeldefs.defs;
+
+import cern.accsoft.steering.jmad.domain.beam.Beam;
+import cern.accsoft.steering.jmad.domain.file.CallableModelFile.ParseType;
+import cern.accsoft.steering.jmad.domain.file.CallableModelFileImpl;
+import cern.accsoft.steering.jmad.domain.file.ModelFile;
+import cern.accsoft.steering.jmad.domain.file.ModelFile.ModelFileLocation;
+import cern.accsoft.steering.jmad.domain.file.ModelPathOffsetsImpl;
+import cern.accsoft.steering.jmad.domain.machine.RangeDefinitionImpl;
+import cern.accsoft.steering.jmad.domain.machine.SequenceDefinitionImpl;
+import cern.accsoft.steering.jmad.domain.twiss.TwissInitialConditionsImpl;
+import cern.accsoft.steering.jmad.modeldefs.ModelDefinitionFactory;
+import cern.accsoft.steering.jmad.modeldefs.domain.JMadModelDefinition;
+import cern.accsoft.steering.jmad.modeldefs.domain.JMadModelDefinitionImpl;
+import cern.accsoft.steering.jmad.modeldefs.domain.OpticsDefinition;
+import cern.accsoft.steering.jmad.modeldefs.domain.OpticsDefinitionImpl;
+
+/**
+ * the model definition for med-austron
+ * 
+ * @author kfuchsbe
+ */
+public class MedAustronModelDefinitionFactory implements ModelDefinitionFactory {
+
+    @Override
+    public JMadModelDefinition create() {
+        JMadModelDefinitionImpl modelDefinition = new JMadModelDefinitionImpl();
+        modelDefinition.setName("MedAustron Synchrotron");
+
+        ModelPathOffsetsImpl offsets = new ModelPathOffsetsImpl();
+        offsets.setResourceOffset("ma");
+        modelDefinition.setModelPathOffsets(offsets);
+
+        modelDefinition.addInitFile(new CallableModelFileImpl("elements.def", ModelFileLocation.RESOURCE));
+        modelDefinition.addInitFile(new CallableModelFileImpl("mr-elements.def", ModelFileLocation.RESOURCE));
+        modelDefinition.addInitFile(new CallableModelFileImpl("mr.seq", ModelFileLocation.RESOURCE));
+
+        /*
+         * OPTICS
+         */
+        OpticsDefinition opticsDefinition = new OpticsDefinitionImpl("default optics", new ModelFile[] {
+                new CallableModelFileImpl("mr-extr.str", ModelFileLocation.RESOURCE, ParseType.STRENGTHS),
+                new CallableModelFileImpl("mr-corrector.str", ModelFileLocation.RESOURCE, ParseType.STRENGTHS) });
+        modelDefinition.setDefaultOpticsDefinition(opticsDefinition);
+
+        /*
+         * SEQUENCE
+         */
+
+        /* NOTE: sequenceName must correspond to the name in .seq - file! */
+        SequenceDefinitionImpl mr = new SequenceDefinitionImpl("mr", new Beam());
+        modelDefinition.setDefaultSequenceDefinition(mr);
+        RangeDefinitionImpl mrRange = new RangeDefinitionImpl(mr, "ALL", new TwissInitialConditionsImpl());
+        mr.setDefaultRangeDefinition(mrRange);
+
+        return modelDefinition;
+    }
+}
