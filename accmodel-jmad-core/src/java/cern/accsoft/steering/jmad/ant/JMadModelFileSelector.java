38b471497476056fb11dbc7b8e51003a452b96ad
Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
2011-07-14 09:55:41 +0000

    added javadoc headers for GPL v3
    git-svn-id: svn+ssh://svn.cern.ch/reps/acc-co/trunk/accmodel/jmad@196869 e02d3557-2f25-4247-9e18-1207d715a173

--- accmodel-jmad-core/src/java/cern/accsoft/steering/jmad/ant/JMadModelFileSelector.java
+++ accmodel-jmad-core/src/java/cern/accsoft/steering/jmad/ant/JMadModelFileSelector.java
@@ -1,6 +1,25 @@
-/**
+// @formatter:off
+ /*******************************************************************************
+ *
+ * This file is part of JMad.
  * 
- */
+ * Copyright (c) 2008-2011, CERN. All rights reserved.
+ * 
+ * JMad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JMad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with JMad.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ******************************************************************************/
+// @formatter:on
 package cern.accsoft.steering.jmad.ant;
 
 import static cern.accmodel.commons.util.ResourceUtil.prependPathOffset;
@@ -74,8 +93,8 @@ public class JMadModelFileSelector extends BaseSelector {
                     /* 1) the name itself */
                     String resourcePath = modelFile.getName();
                     /* 2) the model definition - dependent offset */
-                    resourcePath = prependPathOffset(resourcePath, modelFile.getLocation().getPathOffset(
-                            modelDefinition.getModelPathOffsets()));
+                    resourcePath = prependPathOffset(resourcePath,
+                            modelFile.getLocation().getPathOffset(modelDefinition.getModelPathOffsets()));
 
                     foundFileNames.add(resourcePath);
                 }
--- accmodel-jmad-core/src/java/cern/accsoft/steering/jmad/domain/elem/Position.java
+++ accmodel-jmad-core/src/java/cern/accsoft/steering/jmad/domain/elem/Position.java
@@ -1,3 +1,25 @@
+// @formatter:off
+ /*******************************************************************************
+ *
+ * This file is part of JMad.
+ * 
+ * Copyright (c) 2008-2011, CERN. All rights reserved.
+ * 
+ * JMad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JMad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with JMad.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ******************************************************************************/
+// @formatter:on
 package cern.accsoft.steering.jmad.domain.elem;
 
 public class Position {
