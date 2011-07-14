550d24407f265ed9d1f34ea9c0482d85386c64d0
Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
2011-07-14 16:25:10 +0000

    git-svn-id: svn+ssh://svn.cern.ch/reps/acc-co/trunk/accmodel/jmad@196946 e02d3557-2f25-4247-9e18-1207d715a173

--- accmodel-jmad-core/src/java/cern/accsoft/steering/jmad/util/ZipUtil.java
+++ accmodel-jmad-core/src/java/cern/accsoft/steering/jmad/util/ZipUtil.java
@@ -3,7 +3,7 @@
  *
  * This file is part of JMad.
  * 
- * Copyright (c) 2008-2011, CERN. All rights reserved.
+ * Copyright (c) 2008-2011, Kajetan Fuchsberger. All rights reserved.
  * 
  * JMad is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
