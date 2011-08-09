41e9196b39cd33390387f6e842f17802f6844bee
Gabriel Johannes Mueller <Gabriel.Johannes.Mueller@cern.ch>
2011-08-09 07:18:23 +0000

    - remove twiss listener from previous initial conditions during setTwissInitialConditions
    git-svn-id: svn+ssh://svn.cern.ch/reps/acc-co/trunk/accmodel/jmad@200076 e02d3557-2f25-4247-9e18-1207d715a173

--- accmodel-jmad-core/src/java/cern/accsoft/steering/jmad/model/JMadModelImpl.java
+++ accmodel-jmad-core/src/java/cern/accsoft/steering/jmad/model/JMadModelImpl.java
@@ -1,22 +1,11 @@
 // @formatter:off
- /*******************************************************************************
- *
- * This file is part of JMad.
- * 
- * Copyright (c) 2008-2011, CERN. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+/*******************************************************************************
+ * This file is part of JMad. Copyright (c) 2008-2011, CERN. All rights reserved. Licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in
+ * writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
- * 
  ******************************************************************************/
 // @formatter:on
 
@@ -856,6 +845,7 @@ public class JMadModelImpl implements JMadModel, ElementAttributeReader {
     }
 
     private void setTwissInitialConditions(TwissInitialConditions twissInitialConditions) {
+        this.twissInitialConditions.removeListener(this.twissListener);
         this.twissInitialConditions = twissInitialConditions;
         this.twissInitialConditions.addListener(this.twissListener);
     }
@@ -1013,8 +1003,8 @@ public class JMadModelImpl implements JMadModel, ElementAttributeReader {
         for (MatchConstraint mC : resultRequest.getMatchConstraints()) {
             if (mC.isGlobal()) {
                 for (String gP : mC.getParameterSettings().keySet()) {
-                    MatchConstraintResultGlobal mCrG = new MatchConstraintResultGlobal(gP,
-                            twissSummary.getDoubleValue(gP));
+                    MatchConstraintResultGlobal mCrG = new MatchConstraintResultGlobal(gP, twissSummary
+                            .getDoubleValue(gP));
                     mCrG.setTargetValue(mC.getParameterSettings().get(gP));
                     matchResult.addConstrainParameterResult(mCrG);
                 }
