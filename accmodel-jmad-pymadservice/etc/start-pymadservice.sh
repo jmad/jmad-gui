14ab56b16e8a153b52225218d93a7de606fb7fb7
Yngve Inntjore Levinsen <Yngve.Inntjore.Levinsen@cern.ch>
2011-08-21 22:29:47 +0000

    fixed start-pymadservice.sh for Unix systems
    git-svn-id: svn+ssh://svn.cern.ch/reps/acc-co/trunk/accmodel/jmad@200907 e02d3557-2f25-4247-9e18-1207d715a173

--- accmodel-jmad-pymadservice/etc/start-pymadservice.sh
+++ accmodel-jmad-pymadservice/etc/start-pymadservice.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-java -jar accmodel-jmad-pymadservice.jar
+jname=accmodel-jmad-pymadservice.jar
+
+# first some default paths,
+# then some wild guesses..
+for d in ${JMAD_HOME} ${JAVA_HOME} /usr/lib/java /usr/share/java/ /opt/java/lib/
+do
+    if [ -f "${d}${jname}" ]
+    then
+        jarfile=${d}${jname}
+    fi
+done
+# if still not defined, hoping for CLASSPATH..
+if [ ! -n "$jarfile" ]
+then
+    jarfile=${jname}
+fi
+
+echo "java -jar ${jarfile}"
