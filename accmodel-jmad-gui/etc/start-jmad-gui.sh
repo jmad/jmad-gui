1bf4b196c82b16ab87c039344819d6d27bfc463c
Yngve Inntjore Levinsen <Yngve.Inntjore.Levinsen@cern.ch>
2011-08-28 13:23:00 +0200

    added start-jmad.sh
    modified start-jmad-gui.sh and start-jmad-pymadservice.sh
    to use start-jmad.sh
    
    added jmad.desktop, useful for installation on unix systems
    (perhaps only linux, I'm not sure)

--- accmodel-jmad-gui/etc/start-jmad-gui.sh
+++ accmodel-jmad-gui/etc/start-jmad-gui.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
-jname=accmodel-jmad-gui.jar
+# location where start-jmad.sh is placed:
+scriptloc=''
 
-# first some default paths,
-# then some wild guesses..
-for d in ${JMAD_HOME} ${JAVA_HOME} /usr/lib/java /usr/share/java/ /opt/java/lib/
-do
-    if [ -f "${d}${jname}" ]
-    then
-        jhome=${d}
-    fi
-done
-
-if [ ! -f "${jhome}${jname}" ]
-then
-    echo "Could not find $jname"
-    exit 1
-fi
-
-if [ $jhome ]
-then
-    cd $jhome
-fi
-
-java -Dcern.jmad.output.path=/tmp/ -Dpymad.service.ready.file=/tmp/pymad-service-is-ready.txt -jar ${jname} > /dev/null 
-
+${scriptloc}start-jmad.sh gui $@
--- accmodel-jmad-pymadservice/etc/start-jmad-pymadservice.sh
+++ accmodel-jmad-pymadservice/etc/start-jmad-pymadservice.sh
@@ -1,38 +1,6 @@
 #!/bin/bash
 
-jname=accmodel-jmad-pymadservice.jar
+# location where start-jmad.sh is placed:
+scriptloc=''
 
-# Check correct number of input arguments:
-
-if (( $# != 1 ))
-then
-    echo "using default ready-file"
-    readyfile=pymad-service-ready.out
-else
-	readyfile=$1
-fi
-
-# first some default paths,
-# then some wild guesses..
-for d in ${JMAD_HOME} ${JAVA_HOME} /usr/lib/java /usr/share/java/ /opt/java/lib/
-do
-    if [ -f "${d}${jname}" ]
-    then
-        jhome=${d}
-    fi
-done
-
-if [ ! -f "${jhome}${jname}" ]
-then
-    echo "Could not find $jname"
-    exit 1
-fi
-
-if [ $jhome ]
-then
-    cd $jhome
-fi
-
-clp=$CLASSPATH:${jhome}*;${jhome}lib/*
-
-java -Dpymad.service.ready.file=$readyfile -classpath "$clp" cern.accmodel.jmad.pymadservice.PyMadService > /dev/null 
+${scriptloc}start-jmad.sh pymad $@
