d04c2b310f6d8d8eb96e3e1037881bf9cb7f3cc7
Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
2011-08-28 13:19:29 +0000

    resolved conflict in start-pymadservice.sh
    added start-jmad.sh
    
    modified start-jmad-gui.sh and start-jmad-pymadservice.sh
    to use start-jmad.sh
    
    added jmad.desktop, useful for installation on unix systems
    (perhaps only linux, I'm not sure)
    
    Conflicts:
    
    	accmodel-jmad-pymadservice/etc/start-pymadservice.sh
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/acc-co/trunk/accmodel/jmad@201582 e02d3557-2f25-4247-9e18-1207d715a173

--- accmodel-jmad-pymadservice/etc/start-pymadservice.sh
+++ accmodel-jmad-pymadservice/etc/start-pymadservice.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 
-jname=accmodel-jmad-pymadservice.jar
+# location where start-jmad.sh is placed:
+scriptloc=''
 
-# Check correct number of input arguments:
+${scriptloc}start-jmad.sh pymad $@
 
-if (( $# != 1 ))
-then
-    echo "Wrong number of input arguments"
-    exit 1
-fi
-
-# first some default paths,
-# then some wild guesses..
-for d in ${JMAD_HOME} ${JAVA_HOME} /usr/lib/java /usr/share/java/ /opt/java/lib/
-do
-    if [ -f "${d}${jname}" ]
-    then
-        jhome=${d}
-    fi
-done
-
-if [ ! -f "${jhome}${jname}" ]
-then
-    echo "Could not find $jname"
-    exit 1
-fi
-
-if [ $jhome ]
-then
-    cd $jhome
-fi
-
-java -Dcern.jmad.output.path=/tmp/ -Dpymad.service.ready.file=$1 -jar ${jname} > /dev/null 
