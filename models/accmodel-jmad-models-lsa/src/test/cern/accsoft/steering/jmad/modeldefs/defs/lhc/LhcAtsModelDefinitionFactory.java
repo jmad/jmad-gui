020b76801d773e29b0efbddd8761b5e556b57f3c
Gabriel Johannes Mueller <Gabriel.Johannes.Mueller@cern.ch>
2011-10-17 12:48:46 +0000

    - added new ATS squeeze optics to 10cm in IP1/5
    git-svn-id: svn+ssh://svn.cern.ch/reps/acc-co/trunk/accmodel/jmad@206100 e02d3557-2f25-4247-9e18-1207d715a173

--- models/accmodel-jmad-models-lsa/src/test/cern/accsoft/steering/jmad/modeldefs/defs/lhc/LhcAtsModelDefinitionFactory.java
+++ models/accmodel-jmad-models-lsa/src/test/cern/accsoft/steering/jmad/modeldefs/defs/lhc/LhcAtsModelDefinitionFactory.java
@@ -62,8 +62,8 @@ public class LhcAtsModelDefinitionFactory extends AbstractLhcModelDefinitionFact
         OpticDefinitionSetBuilder builder = OpticDefinitionSetBuilder.newInstance();
 //        builder.addOptic("A1100C1100A1000L1000_2011_ATS_INJ", OpticModelFileBuilder
 //                .createInstance("ATS_V6.503/OPTICS_MD2011/opticsfile.1"));
-//        builder.addOptic("A1100C1100A1000L1000_2011_ATS", OpticModelFileBuilder
-//                .createInstance("ATS_V6.503/OPTICS_MD2011/opticsfile.2"));
+        builder.addOptic("A1100C1100A1000L1000_2011_ATS", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_MD2011/opticsfile.2"));
         builder.addOptic("A1000C1000A1000L1000_2011_ATS", OpticModelFileBuilder
                 .createInstance("ATS_V6.503/OPTICS_MD2011/opticsfile.3"));
         builder.addOptic("A800C800A1000L1000_2011_ATS", OpticModelFileBuilder
@@ -126,13 +126,72 @@ public class LhcAtsModelDefinitionFactory extends AbstractLhcModelDefinitionFact
         OpticDefinitionSetBuilder builder = OpticDefinitionSetBuilder.newInstance();
         
         /* new injection optics -- only the factors changed to the tele-knobs */
-        builder.addOptic("A1100C1100A1000L1000_2011_ATS_INJ", OpticModelFileBuilder
+        builder.addOptic("A1100C1100A1000L1000_2011_ATS_MD4_INJ", OpticModelFileBuilder
                 .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.1"));
         
-        builder.addOptic("A1100C1100A1000L1000_2011_ATS", OpticModelFileBuilder
+        builder.addOptic("A1100C1100A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
                 .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.2"));
+        builder.addOptic("A1000C1000A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.3"));
+        builder.addOptic("A800C800A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.4"));
+        builder.addOptic("A600C600A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.5"));
+        builder.addOptic("A440C440A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.6"));
+        builder.addOptic("A300C300A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.7"));
+        builder.addOptic("A240C240A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.8"));
+        builder.addOptic("A200C200A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.9"));
+        builder.addOptic("A160C160A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.10"));
+        builder.addOptic("A120C120A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.11"));
+        builder.addOptic("A100C100A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.12"));
+        builder.addOptic("A080C080A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.13"));
+        builder.addOptic("A060C060A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.14"));
+        builder.addOptic("A050C050A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.15"));
+        builder.addOptic("A040C040A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.16"));
+        builder.addOptic("A039C039A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.17"));
+        builder.addOptic("A038C038A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.18"));
+        builder.addOptic("A037C037A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.19"));
+        builder.addOptic("A036.5C036.5A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.20"));
+        builder.addOptic("A036C036A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.21"));
+        builder.addOptic("A035C035A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.22"));
+        builder.addOptic("A032C032A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.23"));
+        builder.addOptic("A028C028A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.24"));
+        builder.addOptic("A024C024A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.25"));
+        builder.addOptic("A020C020A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.26"));
+        builder.addOptic("A018C018A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.27"));
+        builder.addOptic("A016C016A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.28"));
+        builder.addOptic("A014C014A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.29"));
+        builder.addOptic("A012C012A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.30"));
+        builder.addOptic("A010C010A1000L1000_2011_ATS_MD4", OpticModelFileBuilder
+                .createInstance("ATS_V6.503/OPTICS_round_IR1_40-10_IR5_40-10/opticsfile.31"));
         
         return builder.build();
     }
     
 }
+
