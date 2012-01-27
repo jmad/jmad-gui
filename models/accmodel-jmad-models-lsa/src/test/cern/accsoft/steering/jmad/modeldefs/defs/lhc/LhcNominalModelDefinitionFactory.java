9c989a9d92ac75fba1d84aeb2ce3cf5a12ef2c6f
Stefano Redaelli <Stefano.Redaelli@cern.ch>
2012-01-27 12:06:32 +0000

    Additional intermediate optics for 2012 commissioning (still with 2011 sequences) - one optics more
    git-svn-id: svn+ssh://svn.cern.ch/reps/acc-co/trunk/accmodel/jmad@215065 e02d3557-2f25-4247-9e18-1207d715a173

--- models/accmodel-jmad-models-lsa/src/test/cern/accsoft/steering/jmad/modeldefs/defs/lhc/LhcNominalModelDefinitionFactory.java
+++ models/accmodel-jmad-models-lsa/src/test/cern/accsoft/steering/jmad/modeldefs/defs/lhc/LhcNominalModelDefinitionFactory.java
@@ -879,6 +879,11 @@ public class LhcNominalModelDefinitionFactory extends AbstractLhcModelDefinition
             OpticModelFileBuilder.createInstance("IR5/IP5_beta_1.60.str"),
             OpticModelFileBuilder.createInstance("IR8/3.5TeV/special/ip8_0.00875_beta3.00m.str"),
             OpticModelFileBuilder.createInstance("IR2/3.5TeV/special/ip2_0.00889_beta3.50m.str") });
+        builder.addOptic("A400C400A600_0.00889L500_0.00900_2011", new OpticModelFileBuilder[] {
+            OpticModelFileBuilder.createInstance("IR1/IP1_beta_4.00.str"),
+            OpticModelFileBuilder.createInstance("IR5/IP5_beta_4.00.str"),
+            OpticModelFileBuilder.createInstance("IR8/3.5TeV/special/ip8_0.00900_beta5.00m.str"),
+            OpticModelFileBuilder.createInstance("IR2/3.5TeV/special/ip2_0.00889_beta6.00m.str") });
 
         return builder.build();
 
