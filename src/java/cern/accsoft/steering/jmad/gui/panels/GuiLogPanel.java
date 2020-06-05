738d4ba4c6a6a6595618f58becbb3237082f8db5
mihostet <michi.hostettler@cern.ch>
2020-06-05 12:25:41 +0200

    support MAD-X COLLIMATOR type; improve logging in GUI

--- src/java/cern/accsoft/steering/jmad/gui/panels/GuiLogPanel.java
+++ src/java/cern/accsoft/steering/jmad/gui/panels/GuiLogPanel.java
@@ -1,5 +1,15 @@
 package cern.accsoft.steering.jmad.gui.panels;
 
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import java.awt.*;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import cern.accsoft.steering.jmad.gui.executor.ActiveJobsEvent;
 import com.google.common.collect.ImmutableMap;
 import org.apache.log4j.Appender;
@@ -9,23 +19,6 @@ import org.apache.log4j.PatternLayout;
 import org.apache.log4j.spi.LoggingEvent;
 import org.springframework.context.event.EventListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.JTextPane;
-import javax.swing.SwingUtilities;
-import javax.swing.Timer;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 /**
  * Panel that includes a logger appender for displaying the logs in the GUI
  */
@@ -34,7 +27,7 @@ public class GuiLogPanel extends JPanel {
     private static final String HIDE_LOGS = "Hide logs";
     private static final int EXPANDED_HEIGHT = 300;
     private static final int CLOSED_HEIGHT = 0;
-    private static final Color DEFAULT_BACKGROUND_COLOR = Color.white;
+    private static final Color DEFAULT_BACKGROUND_COLOR = Color.WHITE;
     private final GuiLogAppender guiLogAppender;
     private final AtomicBoolean isExpanded = new AtomicBoolean(false);
     private final JLabel activeJobsLabel;
@@ -52,7 +45,7 @@ public class GuiLogPanel extends JPanel {
         JButton showLogsButton = new JButton(SHOW_LOGS);
         showLogsButton.setPreferredSize(new Dimension(120, showLogsButton.getPreferredSize().height));
         showLogsButton.addActionListener(c -> {
-            if(isExpanded.get()) {
+            if (isExpanded.get()) {
                 setHeight(fullLogPane, CLOSED_HEIGHT);
                 showLogsButton.setText(SHOW_LOGS);
             } else {
@@ -98,14 +91,14 @@ public class GuiLogPanel extends JPanel {
     }
 
     private static Color backgroundForActiveJobsCount(int activeJobs) {
-        if(activeJobs == 0) {
+        if (activeJobs == 0) {
             return Color.GRAY;
         }
         return Color.BLUE.brighter();
     }
 
     private static String formatActiveJobsCount(int count) {
-        if(count == 0) {
+        if (count == 0) {
             return "No active jobs";
         }
 
@@ -122,13 +115,23 @@ public class GuiLogPanel extends JPanel {
     }
 
     public static class GuiLogAppender extends AppenderSkeleton {
-        private static final Map<Level, Color> LEVEL_COLOR = ImmutableMap
-                .of(Level.ERROR, Color.red, Level.INFO, Color.green, Level.WARN, Color.orange);
+        private static final Map<Level, Color> LEVEL_STATUS_COLOR = ImmutableMap.of( //
+                Level.ERROR, Color.RED, //
+                Level.INFO, Color.GREEN, //
+                Level.WARN, Color.ORANGE);
+        private static final Map<Level, Color> LEVEL_TEXT_COLOR = ImmutableMap.of( //
+                Level.ERROR, Color.RED, //
+                Level.INFO, Color.BLUE, //
+                Level.WARN, Color.ORANGE.darker());
+
         private static final int BACKGROUND_RESET_TIMEOUT_MS = 4000;
 
         private final JTextField lastEventPane;
         private final JTextPane fullEventPane;
         private final Timer lastEventBackgroundResetTime;
+
+        private final Map<Level, Style> levelStyles;
+        private final Style defaultStyle;
 
         GuiLogAppender(JTextField lastEventPane, JTextPane fullEventPane) {
             super(true);
@@ -136,9 +139,20 @@ public class GuiLogPanel extends JPanel {
             this.lastEventPane = lastEventPane;
             this.fullEventPane = fullEventPane;
 
+            ImmutableMap.Builder<Level, Style> levelStyleBuilder = ImmutableMap.builder();
+            LEVEL_TEXT_COLOR.forEach((level, color) -> {
+                Style style = fullEventPane.addStyle(level.toString(), null);
+                StyleConstants.setForeground(style, color);
+                levelStyleBuilder.put(level, style);
+            });
+            this.levelStyles = levelStyleBuilder.build();
+            Style style = fullEventPane.addStyle("DEFAULT", null);
+            StyleConstants.setForeground(style, Color.BLACK);
+            this.defaultStyle = style;
+
             lastEventPane.setBorder(BorderFactory.createEmptyBorder());
 
-            setThreshold(Level.DEBUG);
+            setThreshold(Level.INFO);
             setLayout(new PatternLayout("%d{ABSOLUTE} %-5p [%t]: %m %n"));
 
             lastEventBackgroundResetTime = new Timer(BACKGROUND_RESET_TIMEOUT_MS,
@@ -148,14 +162,23 @@ public class GuiLogPanel extends JPanel {
         @Override
         protected void append(LoggingEvent event) {
             String msg = layout.format(event);
+            String exceptionInfo = Optional.ofNullable(event.getThrowableStrRep()) //
+                    .map(lines -> String.join("\n", lines)) //
+                    .orElse("");
             Level level = event.getLevel();
             /* It's in theory possible that the logs messages are printed not in order. */
             /* It's ok since they are just informative and the overhead would be a bit too much */
             SwingUtilities.invokeLater(() -> {
                 lastEventBackgroundResetTime.restart();
                 lastEventPane.setText(msg);
-                fullEventPane.setText(fullEventPane.getText() + msg);
-                lastEventPane.setBackground(LEVEL_COLOR.getOrDefault(level, DEFAULT_BACKGROUND_COLOR));
+                Document document = fullEventPane.getDocument();
+                try {
+                    document.insertString(document.getLength(), msg + exceptionInfo,
+                            levelStyles.getOrDefault(level, defaultStyle));
+                } catch (BadLocationException e) {
+                    e.printStackTrace(); /* on purpose, do not use logger to prevent infinite loops! */
+                }
+                lastEventPane.setBackground(LEVEL_STATUS_COLOR.getOrDefault(level, DEFAULT_BACKGROUND_COLOR));
             });
         }
 
