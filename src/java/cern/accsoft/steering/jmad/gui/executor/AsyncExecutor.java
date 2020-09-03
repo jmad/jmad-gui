018a6adc15aa975f55c429ba19e7bcb9988b1e4c
mihostet <michi.hostettler@cern.ch>
2020-09-03 22:17:45 +0200

    fix reading and writing of TableModelFile

--- src/java/cern/accsoft/steering/jmad/gui/executor/AsyncExecutor.java
+++ src/java/cern/accsoft/steering/jmad/gui/executor/AsyncExecutor.java
@@ -1,15 +1,15 @@
 package cern.accsoft.steering.jmad.gui.executor;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationEventPublisher;
-
 import java.time.Instant;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEventPublisher;
 
 /**
  * Wrapper around the execution of tasks asynchronously.
@@ -19,7 +19,8 @@ public class AsyncExecutor {
     private static final Logger LOGGER = LoggerFactory.getLogger(AsyncExecutor.class);
     private static final int JOBS_WARN_LEVEL = 20;
 
-    private final ExecutorService executor = Executors.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("async-executor-%d").build());
+    private final ExecutorService executor = Executors
+            .newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("async-executor-%d").build());
     private final AtomicInteger jobsCount = new AtomicInteger(0);
     private final ApplicationEventPublisher eventPublisher;
 
@@ -32,18 +33,24 @@ public class AsyncExecutor {
     }
 
     public void submitAsync(String name, Runnable job) {
-        if(jobsCount.get() >= JOBS_WARN_LEVEL) {
+        if (jobsCount.get() >= JOBS_WARN_LEVEL) {
             LOGGER.warn("Active jobs count >= {}. No limit is enforced, but performance may suffer", JOBS_WARN_LEVEL);
         }
 
-        LOGGER.debug("Starting async job {}", name);
+        LOGGER.info("Starting job '{}' ...", name);
         publishJobsCount(jobsCount.incrementAndGet());
         CompletableFuture<Void> future = CompletableFuture.runAsync(job, executor);
-        future.whenComplete((res, exc) -> publishJobsCount(jobsCount.decrementAndGet()));
+        future.whenComplete((res, exc) -> {
+            publishJobsCount(jobsCount.decrementAndGet());
+            if (exc == null) {
+                LOGGER.info("Job '{}' completed.", name);
+            } else {
+                LOGGER.error("Error in job '{}': {}", name, exc.getMessage(), exc);
+            }
+        });
     }
 
     private void publishJobsCount(int count) {
-        LOGGER.info("Active jobs count {}", count);
         eventPublisher.publishEvent(new ActiveJobsEvent(count));
     }
 
